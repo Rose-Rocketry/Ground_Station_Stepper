4b6d54f7d13abe8f6668982681683918c667ef29
garrett <garrett7621@gmail.com>
2022-04-03 01:11:10 -0400

    break up code into functions to call from api

--- Stepper_Motor_test.py
+++ Stepper_Motor_test.py
@@ -1,31 +1,55 @@
 from time import sleep
 import RPi.GPIO as GPIO
-DIR = 17
-STEP = 27
-CW =1
-CCW =0
-SPR = 200 #steps per revolution (360/1.8)
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(DIR, GPIO.OUT)
-GPIO.setup(STEP, GPIO.OUT)
-GPIO.output(DIR, CW)
 
-step_count = SPR
-delay = .108
+def setup_yagi():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(DIR, GPIO.OUT)
+    GPIO.setup(STEP, GPIO.OUT)
+    GPIO.output(DIR, CW)
 
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+def rotate_yagi():
+"""
+Rotate the yagi one rotation and then roate it back
+"""
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
 
-sleep(.5)
-GPIO.output(DIR, CCW)
-for x in range(step_count):
-    GPIO.output(STEP, GPIO.HIGH)
-    sleep(delay)
-    GPIO.output(STEP, GPIO.LOW)
-    sleep(delay)
+    sleep(.5)
+    GPIO.output(DIR, CCW)
+    for x in range(step_count):
+        GPIO.output(STEP, GPIO.HIGH)
+        sleep(delay)
+        GPIO.output(STEP, GPIO.LOW)
+        sleep(delay)
 
-GPIO.cleanup()
+def on_end(inturrupted):
+"""
+Call this when the api is done with us or when we lose connection or in some other way are inturrepted
+"""
+    GPIO.cleanup()
+    
+
+if __name__ == "__main__":
+"""
+Main program here
+"""
+
+    DIR = 17
+    STEP = 27
+    CW =1
+    CCW =0
+    SPR = 200 #steps per revolution (360/1.8)
+    
+    
+    delay = .108
+    step_count = SPR
+    setup_yagi()
+
+    rotate_yagi()
+
+    on_end()
+
